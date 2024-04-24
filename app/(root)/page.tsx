8b0083135f0bc59b8ce96c3cbdb0c5278640c48c
unknown <khushi.chhetri.0000@gmail.com>
2024-04-24 19:23:12 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+      {/* <UserButton /> */}
+      <p>Home</p>
+      
+      </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//eachtransformations will have its own id:localhost:3000/transformations/[id]
